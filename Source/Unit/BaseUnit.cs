c6d7c43c3f1b38e8e148825122ee130ba571443f
cheetahspots01 <112513827+752986@users.noreply.github.com>
2022-10-21 21:16:35 -0600

    Expand BaseUnit; Start figuring out orders

--- Source/Unit/BaseUnit.cs
+++ Source/Unit/BaseUnit.cs
@@ -5,5 +5,8 @@ namespace CSRTS.Source.Unit {
 	internal abstract class BaseUnit {
 		public float Speed { get; protected init; }
 		public Vector2f Position { get; protected set; } = new();
+		public int Health { get; set; }
+		public Queue<Order> Orders { get; set; }
+		public FiringStance Stance { get; }
 	}
 }
---
+++ Source/Unit/Order.cs
@@ -0,0 +1,8 @@
+namespace CSRTS.Source.Unit {
+	enum FiringStance {
+		Flee,
+		Hold,
+		Return,
+		Attack
+	} 
+}
