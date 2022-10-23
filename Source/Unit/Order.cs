85f1118ba6b8557c4cce66e53982ed507f6b59a3
cheetahspots01 <112513827+752986@users.noreply.github.com>
2022-10-22 19:56:17 -0600

    Create Order data structure and update Unit class

--- Source/Unit/Order.cs
+++ Source/Unit/Order.cs
@@ -1,8 +1,22 @@
+using SFML.System;
+
 namespace CSRTS.Source.Unit {
-	enum FiringStance {
+	enum FireStance {
+		/// <summary> Run away from enemies </summary>
 		Flee,
+		/// <summary> Do not attack enemies </summary>
 		Hold,
+		/// <summary> Attack enemies that attacked first </summary>
 		Return,
-		Attack
-	} 
+		/// <summary> Attack any spotted enemies </summary>
+		AtWill
+	}
+
+	// This is esntially an enum, except:
+	// a) it can be inherited from, so units can add their own order types
+	// b) each order can contain data, such as the spot to move to
+	abstract record Order {
+		public record Move(Vector2f To) : Order;
+		public record Attack(Unit Target) : Order;
+	}
 }
