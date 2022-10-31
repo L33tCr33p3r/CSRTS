86444a5bdd18fef70d0f289a52403c977a840b45
Coryvin Peterson <coryvin@thebandit.org>
2022-10-31 13:53:40 -0600

    THE THING WORKS

--- Client.cs
+++ Client.cs
@@ -1,13 +1,29 @@
-﻿using System.Net.Sockets;
+﻿using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
 
 namespace CSRTS
 {
     internal class Client
     {
-        private TcpClient tcpClient;
+        private TcpClient _tcpClient = new("127.0.0.1", 4503);
         public void Run()
         {
-
+			var stream = _tcpClient.GetStream();
+			while (true)
+			{
+				if (stream.DataAvailable)
+				{
+					byte[] text = new byte[1024];
+					int n = stream.Read(text, 0, _tcpClient.Available);
+					//Console.WriteLine(_tcpClient.Available);
+					Console.WriteLine(Encoding.ASCII.GetString(text));
+				}
+				else
+				{
+					//Console.WriteLine("no bytes available");
+				}
+			}
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
 			Thread ServerThread = new(new ThreadStart(ServerStart));
 			ServerThread.Start();
 			Thread ClientThread = new(new ThreadStart(ClientStart));
+			ClientThread.Start();
         }
 		static void ServerStart()
 		{
